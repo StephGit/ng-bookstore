d351f7f80601426cc6eb489efa6155efa74d6cb0
sg <24672636+StephGit@users.noreply.github.com>
2018-02-17 18:47:19 +0100

    removed deprecated httpModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,14 +13,12 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ThemeModule } from './@theme/theme.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {HttpModule} from '@angular/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpModule,
     AppRoutingModule,
 
     NgbModule.forRoot(),

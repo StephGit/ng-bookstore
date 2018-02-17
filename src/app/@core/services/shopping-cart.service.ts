56ce0f2f60f4b9fd1c0849a9623ca7c746c5cb49
Adrian Krebs <krebs-adrian@bluewin.ch>
2018-02-17 20:43:37 +0100

    feat: save current shopping cart to local storage

--- src/app/@core/services/shopping-cart.service.ts
+++ src/app/@core/services/shopping-cart.service.ts
@@ -1,42 +1,44 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {ApiService} from './api.service';
-import {ShoppingCart} from "../model/shopping-cart.model";
-import {Book} from "../model/book.model";
-import {ShoppingCartItem} from "../model/shopping-cart-item.model";
+import {ShoppingCart} from '../model/shopping-cart.model';
+import {Book} from '../model/book.model';
+import {ShoppingCartItem} from '../model/shopping-cart-item.model';
+import {environment} from '../../../environments/environment';
 
 @Injectable()
 export class ShoppingCartService {
 
   currentShoppingCart: ShoppingCart = new ShoppingCart();
-
-  mockShoppingCart = [{
-    title: 'Java All-in-One For Dummies ',
-    authors: 'Doug Lowe',
-    isbn: '1119247799',
-    price: '39.99',
-  }, {title: 'Java All-in-One For Dummies ', authors: 'Doug Lowe', isbn: '1119247799', price: '39.99'},
-    {
-      title: 'Java All-in-One For Dummies ',
-      authors: 'Doug Lowe',
-      isbn: '1119247799',
-      price: '39.99',
-    }, {title: 'Java All-in-One For Dummies ', authors: 'Doug Lowe', isbn: '1119247799', price: '39.99'}]
+  isMockEnabled = `${environment.mock}`;
+  private STORAGE_KEY: string = 'shopping-cart';
 
   constructor() {
   }
 
   public getCurrentShoppingCart() {
+    if (!this.currentShoppingCart.items || this.currentShoppingCart.items.length === 0) {
+      const cartFromStorage = JSON.parse(localStorage.getItem(this.STORAGE_KEY));
+      if (cartFromStorage && cartFromStorage._items) {
+        const storedShoppingCart = new ShoppingCart();
+        cartFromStorage._items.forEach(i =>
+          storedShoppingCart.addItem(new ShoppingCartItem(
+            new Book(i.book.isbn, i.book.authors, i.book.title, i.book.price), i.quantity),
+          ));
+        this.currentShoppingCart = storedShoppingCart;
+      }
+    }
     return this.currentShoppingCart;
   }
 
   public addItemToShoppingCart(item: ShoppingCartItem) {
     this.currentShoppingCart.addItem(item);
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.currentShoppingCart));
   }
 
   public removeItemFromShoppingCart(item: ShoppingCartItem) {
     this.currentShoppingCart.removeItem(item);
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.currentShoppingCart));
   }
-
 
 
 }

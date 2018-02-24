19902d9e7980945a5e8470ccc61c2bd7f9a833a9
StephGit <24672636+StephGit@users.noreply.github.com>
2018-02-24 19:42:47 +0100

    Updates

--- main.bundle.js
+++ main.bundle.js
@@ -784,7 +784,7 @@ var CustomerService = /** @class */ (function () {
         return body || {};
     };
     CustomerService.prototype.handleError = function (error, method) {
-        this.notificationService.error(this.errorService.getCustomerError([method, error.status]), 'Customer error');
+        this.notificationService.error(this.errorService.getCustomerError({ method: method, status: error.status }), 'Customer error');
         return __WEBPACK_IMPORTED_MODULE_1_rxjs_Observable__["a" /* Observable */].throw(error);
     };
     CustomerService = __decorate([
@@ -827,28 +827,28 @@ var ErrorService = /** @class */ (function () {
         this.setCatalogErrors();
     }
     ErrorService.prototype.setCustomerErrors = function () {
-        this.customerErrors.set(['register', 400], 'Invalid registration data');
-        this.customerErrors.set(['login', 401], 'Email or password is incorrect');
-        this.customerErrors.set(['login', 404], 'Email or password is incorrect');
-        this.customerErrors.set(['changePassword', 404], 'Email is incorrect');
-        this.customerErrors.set(['find', 404], 'No customer with this number exists');
-        this.customerErrors.set(['update', 400], 'Invalid data');
-        this.customerErrors.set(['update', 404], 'No customer with this number exists');
-        this.customerErrors.set(['update', 409], 'Email is allready used by a customer');
+        this.customerErrors.set({ method: 'register', status: 400 }, 'Invalid registration data');
+        this.customerErrors.set({ method: 'login', status: 401 }, 'Email or password is incorrect');
+        this.customerErrors.set({ method: 'login', status: 404 }, 'Email or password is incorrect');
+        this.customerErrors.set({ method: 'changePassword', status: 404 }, 'Email is incorrect');
+        this.customerErrors.set({ method: 'find', status: 404 }, 'No customer with this number exists');
+        this.customerErrors.set({ method: 'update', status: 400 }, 'Invalid data');
+        this.customerErrors.set({ method: 'update', status: 404 }, 'No customer with this number exists');
+        this.customerErrors.set({ method: 'update', status: 409 }, 'Email is allready used by a customer');
     };
     ErrorService.prototype.setOrderErrors = function () {
-        this.orderErrors.set(['search', 404], 'No orders found.');
-        this.orderErrors.set(['place', 402], 'Payment error occurred');
-        this.orderErrors.set(['place', 404], 'The order referenced a non existing book or customer');
-        this.orderErrors.set(['cancel', 404], 'No order with the specified number exists.');
-        this.orderErrors.set(['cancel', 409], 'The order has allready been shipped');
-        this.orderErrors.set(['find', 404], 'No order with the specified number exists.');
+        this.orderErrors.set({ method: 'search', status: 404 }, 'No orders found.');
+        this.orderErrors.set({ method: 'place', status: 402 }, 'Payment error occurred');
+        this.orderErrors.set({ method: 'place', status: 404 }, 'The order referenced a non existing book or customer');
+        this.orderErrors.set({ method: 'cancel', status: 404 }, 'No order with the specified number exists.');
+        this.orderErrors.set({ method: 'cancel', status: 409 }, 'The order has allready been shipped');
+        this.orderErrors.set({ method: 'find', status: 404 }, 'No order with the specified number exists.');
     };
     ErrorService.prototype.setCatalogErrors = function () {
-        this.catalogErrors.set(['add', 409], 'A book with the same ISBN number already exists.');
-        this.catalogErrors.set(['find', 404], 'No book with the specified ISBN number exists.');
-        this.catalogErrors.set(['update', 400], 'The ISBN number of the book data does not match the path parameter.');
-        this.catalogErrors.set(['update', 404], 'No book with the specified ISBN number exists.');
+        this.catalogErrors.set({ method: 'add', status: 409 }, 'A book with the same ISBN number already exists.');
+        this.catalogErrors.set({ method: 'find', status: 404 }, 'No book with the specified ISBN number exists.');
+        this.catalogErrors.set({ method: 'update', status: 400 }, 'The ISBN number of the book data does not match the path parameter.');
+        this.catalogErrors.set({ method: 'update', status: 404 }, 'No book with the specified ISBN number exists.');
     };
     ErrorService.prototype.getCustomerError = function (key) {
         console.log(this.customerErrors.get(key));

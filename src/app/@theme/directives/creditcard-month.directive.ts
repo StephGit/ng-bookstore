b1f9bf8563df0d55c19b39d1e071b3f9e6ffd0f1
StephGit <24672636+StephGit@users.noreply.github.com>
2018-03-04 12:38:54 +0100

    fixed validation issues

--- src/app/@theme/directives/creditcard-month.directive.ts
+++ src/app/@theme/directives/creditcard-month.directive.ts
@@ -14,8 +14,9 @@ export class CreditcardMonthDirective implements Validator {
     const currentYear = new Date().getFullYear();
     const currentMonth = new Date().getMonth() + 1;
     const yearControl = c.root.get(this.validateYear);
-    const year = yearControl !== null ? yearControl.value : 0;
-    const isValid = !isNaN(numValue) && (year > currentYear || (year === currentYear && numValue >= currentMonth));
+    const year = (yearControl !== undefined || yearControl !== null) ? yearControl.value : 0;
+    const isValid = !isNaN(numValue) &&
+      (year > currentYear || (year === currentYear && numValue >= currentMonth) || year === 0 || year === undefined);
     const message = {
       'month': {
         'message': 'The value is expired. It must be a valid number between ' + currentMonth + ' and ' + 12,
